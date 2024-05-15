06e570103ac76e0362caa55f7a49536d86a6590f
Sergey N. <sernepochatyh@gmail.com>
2024-05-15 20:06:17 +0300

    Добавил модели

---
+++ CorporatePortal/Models/Entry.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CorporatePortal.Models
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public int Hours { get; set; }
+
+        public string? Description { get; set; }
+
+        public int TaskId { get; set; }
+        [JsonIgnore]
+        public virtual Task Task { get; set; } = null!;
+    }
+}
---
+++ CorporatePortal/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CorporatePortal.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int ProjectId { get; set; }
+
+        public bool IsActive { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Entry> Entries { get; set; } = new List<Entry>();
+        [JsonIgnore]
+        public virtual Project Project { get; set; } = null!;
+    }
+}
