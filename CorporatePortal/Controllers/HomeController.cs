36ef613ad0a9ea1d00a328dd6fa0d1d7adf6b181
Sergey N. <sernepochatyh@gmail.com>
2024-05-16 16:54:27 +0300

    Добавил вывод из базы данных

--- CorporatePortal/Controllers/HomeController.cs
+++ CorporatePortal/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using CorporatePortal.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace CorporatePortal.Controllers
@@ -8,6 +9,10 @@ namespace CorporatePortal.Controllers
     {
 
         private readonly ILogger<HomeController> _logger;
+        private HttpClient _httpClient = new HttpClient()
+        {
+			BaseAddress = new Uri("http://localhost:5125/api/")
+	    };
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -16,23 +21,44 @@ namespace CorporatePortal.Controllers
 
         public IActionResult Index()
         {
-            return View();
+			return View();
         }
 
-        public IActionResult ProjectAccounting()
+        public async Task<IActionResult> ProjectAccounting()
         {
-            return View();
+		    var response = await _httpClient.GetAsync("Projects");
+            if(response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                List<Project> projects = JsonConvert.DeserializeObject<List<Project>>(data);
+                return View(projects);
+            }
+            return View("Error");
         }
 
-        public IActionResult PostingAccounting()
+        public async Task<IActionResult> PostingAccounting()
         {
-            return View();
-        }
+			var response = await _httpClient.GetAsync("Entries");
+			if (response.IsSuccessStatusCode)
+			{
+				string data = await response.Content.ReadAsStringAsync();
+				List<Entry> entries = JsonConvert.DeserializeObject<List<Entry>>(data);
+				return View(entries);
+			}
+			return View("Error");
+		}
 
-        public IActionResult TaskAccounting()
+        public async Task <IActionResult> TaskAccounting()
         {
-            return View();
-        }
+			var response = await _httpClient.GetAsync("Tasks");
+			if (response.IsSuccessStatusCode)
+			{
+				string data = await response.Content.ReadAsStringAsync();
+				List<Models.Task> tasks = JsonConvert.DeserializeObject<List<Models.Task>>(data);
+				return View(tasks);
+			}
+			return View("Error");
+		}
 
         public IActionResult Privacy()
         {
